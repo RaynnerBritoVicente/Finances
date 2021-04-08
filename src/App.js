9f17433502a7b018ec136024b6868cb1a1dd9a55
RaynnerBritoVicente <raynnergames.rbv@outlook.com>
2021-04-08 17:07:39 -0300

    tentativa de implementar a API de cotações

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from '../src/images/Logo_FB.png'
+import Bandeira from '../src/images/bandeira.jpg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from 'react-bootstrap/Navbar'
@@ -8,14 +9,49 @@ import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
 import { RiBarChart2Fill } from "react-icons/ri"
 import { FaSearchDollar } from "react-icons/fa"
 
-
-
 function App(){
+  const [resultado, setResultado] = useState(null)
+  const [corpo, setCorpo] = useState(false)
+  const [valor, setValor] = useState('')
+  const [moeda, setMoeda] = useState('')
+  const [convertido, setConvertido] = useState('')
+
+  async function obtemCotacao(moeda, valor) {
+    let urlCota = `https://economia.awesomeapi.com.br/json/all/`
+    await fetch(urlCota)
+     .then(response => response.json())
+     .then(data => {
+       console.log(data)
+       setResultado(data)
+     })
+     .catch(function(error){
+       console.error(`Sinto muito, ocorreu um erro inesperado. Erro: ${error.message}`)
+     })
+  }
+
+  function converter(valor, moeda, retorno){
+    let setConvertido = valor / retorno[moeda]['ask']
+  }
+
+  useEffect(() => {
+    document.title = "Fatec Bolsas - Home"
+  },[])
+
+  useEffect(() => {
+    if(resultado){
+      setCorpo(true)
+    }else{
+      setCorpo(false)
+    }
+  },[resultado])
 
   return(
     <>
@@ -25,26 +61,65 @@ function App(){
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#dev">Devs</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl placeholder="Pesquisar"/>
-           &nbsp;
-          <Button variant="outline-info"><FaSearchDollar color="yellow"/> Buscar</Button>
-        </Form>
       </Navbar>
       <Jumbotron className="centralizar">
         <h1><RiBarChart2Fill/> Fatec - Bolsas</h1>
         <p>
-          Consulte os altos e baixos da bolsa de valores por aqui
+          Converta todas as moedas para Real Brasileiro
         </p>
       </Jumbotron>
 
-      <div className="conteudo"></div>
-      <div className="conteudo"></div>
-      <div className="conteudo"></div>
-      <div className="conteudo2"></div>
+      <Container>
+        <Row>
+          <Col>
+          <Form>
+            <Form.Group>
+              <Form.Label>Moeda a converter</Form.Label>
+              <Form.Control as="select" onChange={event => setMoeda(event.target.value)}>
+                <option value="" diabled>Selecione uma Moeda</option>
+                <option value="USD">Dólar Americano</option>
+                <option value="CAD">Dólar Canadense</option>
+                <option value="AUD">Dólar Australiano</option>
+                <option value="EUR">Euro</option>
+                <option value="GBP">Libra Esterlina</option>
+                <option value="ARS">Peso Argentino</option>
+                <option value="JPY">Iene Japonês</option>
+                <option value="CHF">Franco Suiço</option>
+                <option value="CNY">Yuan Chinês</option>
+                <option value="YLS">Novo Shekel Israelense</option>
+                <option value="BTC">Bitcon</option>
+                <option value="LTC">Litecoin</option>
+                <option value="ETH">Ethereum</option>
+                <option value="XRP">Ripple</option>
+              </Form.Control>
+          </Form.Group>
+          <Form.Group >
+              <Form.Label>Valor</Form.Label>
+              <Form.Control type="number" onChange={event => setValor(event.target.value)} placeholder="Ex: 300" />
+          </Form.Group>
+          <Form.Group >
+          <Button className="botao_procura" variant="outline-success" onClick={() => obtemCotacao()}><FaSearchDollar color="yellow"/>Converter</Button>
+          </Form.Group>
+        {corpo &&
+          <Card bg="primary" className="text-center">
+            <Card.Header>
+              <h3>Conversão</h3>
+            </Card.Header>
+            <Card.Body className="bg-light">
+              <img src={Bandeira} width="250px" />
+              <h1>R${convertido}</h1>
+            </Card.Body>
+          </Card>
+        }
+          </Form>
+          </Col>
+        </Row>
+      </Container>
 
+      
+        
       <div id="rodape">
-        <p id="conteudo_rodape">Fatec - Bolsas &copy; Todos os Dirteitos Reservados <br/> Desenvolvido por Raynner Brito e Pedro Domingues</p>
+        <p id="conteudo_rodape">Fatec - Bolsas &copy; Todos os Direitos Reservados <br/> Desenvolvido por Raynner Brito e Pedro Domingues</p>
       </div>
 
     </>
